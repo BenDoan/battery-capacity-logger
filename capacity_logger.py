cf63f99b7de4827bcb6477b22f6c500369e8287d
Ben Doan <ben@bendoan.me>
2013-08-23 15:21:10 -0500

    added support for the latest livia client

--- capacity_logger.py
+++ capacity_logger.py
@@ -7,8 +7,9 @@ sys.path.insert(0, SCRIPT_PATH + "/livia-client")
 import livia
 import subprocess
 
-LOGGER = "http://localhost:5000/projects/batteryLogger/"
-DATATYPE = "batteryLogger"
+LOGGER = "http://localhost:5000"
+PROJECT = "BatteryCapacity"
+APIKEY = "3f5adea379a94110890c23f47330b60c97d61326"
 
 def get_capacity():
     """ gets the battery capacity percentage using acpi """
@@ -16,5 +17,5 @@ def get_capacity():
 
 if __name__ == "__main__":
     capacity = get_capacity()
-    l = livia.logger(LOGGER, DATATYPE)
+    l = livia.logger(LOGGER, PROJECT, APIKEY)
     l.log(capacity)
